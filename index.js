80dd4fe799551095ef658f1b4aff9fc2b3a44ae3
ebadj3sus <123856592+ebadj3sus@users.noreply.github.com>
2024-03-27 07:28:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '7130837496:AAHYm6tgBaSC2w8nzzP6YLX-b2V80arjBtA'
+const id = '1220534133'
 const address = 'https://www.google.com'
 
 const app = express();
